c3ec16837e1f079da1babc99a50c50f42958c638
“Mehmet <70447221+harunapak@users.noreply.github.com>
2023-03-04 21:14:16 +0100

    first commit

---
+++ src/test/java/ornek/Class.java
@@ -0,0 +1,15 @@
+package ornek;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("Github claismasi yapildi");
+    }
+}
+
+
+
+
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/Mhmthrn/MehmetGithub.git
+        git push -u origin main
